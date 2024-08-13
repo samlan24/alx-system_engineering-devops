9c87a3d427f19d6c4b220792695ea6d305702933
samlan24 <muchemionline@gmail.com>
2024-08-13 07:39:30 -0700

    initial commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,6 +9,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """returns number of subsbribers"""
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
@@ -17,4 +18,3 @@ def number_of_subscribers(subreddit):
         return data['data']['subscribers']
     else:
         return 0
-
