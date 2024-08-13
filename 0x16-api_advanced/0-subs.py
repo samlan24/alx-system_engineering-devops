6facdcf924c81fe7d8e0b1d5e060bfc214739efa
samlan24 <muchemionline@gmail.com>
2024-08-13 14:15:42 +0000

    initial commit:

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+This script demonstrates how to retrieve data
+from an API using the requests library.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    data = response.json()
+    if response.status_code == 200:
+        return data['data']['subscribers']
+    else:
+        return 0
+
